0344ebb1b2d5843399c09177292df35d57bfb689
Shuvradeb <shuvradeb.saha@ntb.no>
2024-12-17 11:35:03 +0600

    Renamed

--- src/main/java/saha/project/fileencryptor/EncryptorApplication.java
+++ src/main/java/saha/project/fileencryptor/EncryptorApplication.java
@@ -16,7 +16,7 @@ public class EncryptorApplication extends Application {
   @Override
   public void start(Stage stage) throws IOException {
     FXMLLoader fxmlLoader =
-        new FXMLLoader(EncryptorApplication.class.getResource("hello-view.fxml"));
+        new FXMLLoader(EncryptorApplication.class.getResource("encryptor-view.fxml"));
     Scene scene = new Scene(fxmlLoader.load(), 420, 240);
     stage.setTitle("Shuvra's File Encryptor");
     // Set the application icon
